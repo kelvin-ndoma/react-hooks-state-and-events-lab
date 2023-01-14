e802467ff4cb23fc277946fc103fffcc7fe26d0c
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-01-14 22:19:43 +0300

    completed

--- src/data/items.js
+++ src/data/items.js
@@ -4,6 +4,7 @@ const items = [
   { id: 3, name: "Lettuce", category: "Produce" },
   { id: 4, name: "String Cheese", category: "Dairy" },
   { id: 5, name: "Cookies", category: "Dessert" },
+  
 ];
 
 export default items;
